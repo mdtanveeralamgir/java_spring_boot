8760eda634f45839921c3b572152d6c598ae85f2
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-10 22:45:34 -0400

    User class

---
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserDaoService.java
@@ -0,0 +1,10 @@
+package com.in28minutes.rest.webservices.resfulwebservices.user;
+import org.springframework.stereotype.Component;
+
+@Component //Spring to manage this
+
+/*
+DAO: Data Access Object
+*/
+public class UserDaoService {
+}

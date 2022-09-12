60de5d45b64d0dd9a1a3433f031aeb2b8815c9ee
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-11 23:03:37 -0400

    overriding spring errorHandling class and getting default error message for all error type and 404

---
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.in28minutes.rest.webservices.resfulwebservices.exception;
+
+import com.in28minutes.rest.webservices.resfulwebservices.user.UserNotFoundExceltion;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDate;
+
+@ControllerAdvice //Make this class picked up by spring framework
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    //Overriding the method "handleException"from "ResponseEntityExceptionHandler" class
+    @ExceptionHandler(Exception.class) //Override for all exception
+    public final ResponseEntity<ErrorDetails> handleAllException(Exception ex, WebRequest request) throws Exception
+    {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundExceltion.class) //Only for 404 error
+    public final ResponseEntity<ErrorDetails> handleUserNotFound(Exception ex, WebRequest request) throws Exception
+    {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+}

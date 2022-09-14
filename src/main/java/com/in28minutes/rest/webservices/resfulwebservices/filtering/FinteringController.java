a119b423ed8766ef96694dfa849910b8dc821da3
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-13 22:14:10 -0400

    static filtering

--- src/main/java/com/in28minutes/rest/webservices/resfulwebservices/filtering/FinteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/filtering/FinteringController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.resfulwebservices.filtering;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,12 @@ public class FinteringController {
 	public SomeBean filtering()
 	{
 		return new SomeBean("Value_1", "Value_1", "Value_1");
+	}
+	
+	
+	@GetMapping("/filtering_list")
+	public List<SomeBean> filteringList()
+	{
+		return Arrays.asList(new SomeBean("Value_1", "Value_1", "Value_1"), new SomeBean("Value_4", "Value_5", "Value_6"));
 	}
 }

bb9303ba3ff964412f851372b22869ce3e4b2f41
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-26 22:53:03 -0400

    adding filter in functional method

--- src/main/java/functionalProgramming/FP01Functional.java
+++ src/main/java/functionalProgramming/FP01Functional.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 public class FP01Functional {
 
+    private static List<Integer> numbers = List.of(1,3,12,43,23,12,54);
     public static void main(String[] args)
     {
 
-        printAllNumbersInListFunctional(List.of(1,3,12,43,23,12,54));
+//        printAllNumbersInListFunctional(numbers);
+        printAllEvenNumbersInListFunctional(numbers);
     }
 
     //Traditional approach
@@ -17,7 +19,18 @@ public class FP01Functional {
         //Then we can do a "Method reference" on each of that number
         numbers.stream().forEach(FP01Functional::print); //Method reference, defining the behavior
     }
+    private static void printAllEvenNumbersInListFunctional(List<Integer> numbers) {
+        //Adding a filter to print only even numbers
+        numbers
+                .stream()
+                .filter(FP01Functional::isEven)
+                .forEach(System.out::println); //println is a static method inside System.out
+    }
 
+    private static boolean isEven(int number)
+    {
+        return number % 2 == 0;
+    }
     private static void print(int number)
     {
         System.out.println(number);

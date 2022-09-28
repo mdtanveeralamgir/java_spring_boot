23932eeaa02ef5705590a7712ea11181f7ca6160
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2022-09-27 21:27:36 -0400

    applying commandLineRunner and JpaRepository

---
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserCommandLineRunner.java
@@ -0,0 +1,11 @@
+package com.in28minutes.rest.webservices.resfulwebservices.user;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class UserCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
+
---
+++ src/main/java/com/in28minutes/rest/webservices/resfulwebservices/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.in28minutes.rest.webservices.resfulwebservices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class UserRepository extends JpaRepository<User, Integer> {
+}
